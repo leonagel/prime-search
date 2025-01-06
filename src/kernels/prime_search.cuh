297994f2e5c2fc8cf926ae1bf961beb51f904753
Bradley Hsu <warriorhsu@gmail.com>
2025-01-06 16:48:53 +0000

    PROGRESS COMMIT: Finished prime pruning (finally).

--- src/kernels/prime_search.cuh
+++ src/kernels/prime_search.cuh
@@ -3,7 +3,7 @@
 #include <cuda_fp16.h>
 
 // Kernel declaration
-__global__ void prime_search(int* data, int size, int* output);
+__global__ void prime_search(long start_offset, int stride, int* primes_array, int* remainder_array, long* output, int* output_count);
 
 // Wrapper class for kernel management and timing
 class KernelManagerPrimeSearch {
@@ -12,5 +12,8 @@ public:
     static dim3 calculateGrid(int N, int threadsPerBlock);
 
     // Launch kernel with timing
-    static float launchKernel(int* data, int* output);
+    static float launchKernel(long start_offset, int* primes_array, int* remainder_array, long primes_length, long* output, int* output_count);
+
+    // Load in previously attained pruning arrays and return their length
+    static int loadArraysFromCache(int* output_array, int* primes_array, const char* cache_folder);
 };
